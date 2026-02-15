56214ecfd865fea6faaa57beb21c5e3ee4d2efbb
futurewan <393192789@qq.com>
2026-02-15 21:53:19 +0800

    chore: stop tracking dist-electron build directory

--- dist-electron/main.js
+++
@@ -1,9 +0,0 @@
-import "electron";
-import "node:path";
-import "node:url";
-import { M, R, V } from "./main-mgNvtcb5.js";
-export {
-  M as MAIN_DIST,
-  R as RENDERER_DIST,
-  V as VITE_DEV_SERVER_URL
-};
